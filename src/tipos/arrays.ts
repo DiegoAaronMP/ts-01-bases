dd71ded1c0ea8dd8ae7121a70e5ac62f4ae2f474
DiegoAaronMP <daaron.macias@gmail.com>
2024-09-30 16:35:25 -0600

    Arrays - Arreglos

---
+++ src/tipos/arrays.ts
@@ -0,0 +1,19 @@
+
+
+// number[]
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// (string | number)[]
+// const numbers = [1,2,3,4,5,'6',7,8,9,10];
+
+const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10];
+
+numbers.push(11);
+numbers.push('12');
+
+numbers.push(true);
+
+
+const villanos = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+
+villanos.forEach( villano => console.log(villano.toUpperCase()))
