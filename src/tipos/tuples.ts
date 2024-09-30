7cd57739bf1c38fca357f681c6497cff044da701
DiegoAaronMP <daaron.macias@gmail.com>
2024-09-30 16:40:18 -0600

    Tuples - Tuplas

---
+++ src/tipos/tuples.ts
@@ -0,0 +1,17 @@
+
+
+// * El primer valor debe ser un string y el segundo un number
+// const hero: [string, number] = ['Dr Strange' , 100];
+const hero: [string, number, boolean] = ['Dr Strange' , 100, true];
+
+// ! Esto no se puede
+// hero[0] = 50;
+// hero[1] = 'Iron Man';
+
+// * Esto si se puede
+hero[0] = 'Iron Man';
+hero[1] = 50;
+
+hero[2] = false;
+
+console.log(hero);
