7e84a35de486707e7267538149babd5cfe27c263
DiegoAaronMP <daaron.macias@gmail.com>
2024-10-02 17:38:00 -0600

    Tarea y Resolución del Ejercicio #1

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,47 @@
-// (() => {
-    const a:number = 10;
-    
-    let b: string;
-    
-    // ! b = 3.1416;
-    // ! b = {};
-    // ! b = [];
-    b = 'hola';
-    
-    function sayHello( msg: string ) {
-        console.log(msg + ' ' + 'Diego');
+(() => {
+
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    // Enumeraciones
+    enum fuerza {
+      Aquaman = 0,
+      Batman = 1,
+      Flash = 5,
+      Superman = 100,
     }
 
-    console.log(a);
-    sayHello(b);    
-// })();
+    const fuerzaFlash: fuerza = fuerza.Flash;
+    const fuerzaSuperman: fuerza = fuerza.Superman;
+    const fuerzaBatman: fuerza = fuerza.Batman;
+    const fuerzaAquaman: fuerza = fuerza.Aquaman;
+
+  
+    // Retorno de funciones
+    function activar_batiseñal():string {
+      return 'activada';
+    }
+  
+    function pedir_ayuda():void {
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
