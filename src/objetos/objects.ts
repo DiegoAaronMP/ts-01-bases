d0f0041a40bf98c293ce7dc84a0a997f915160a7
DiegoAaronMP <daaron.macias@gmail.com>
2024-10-17 16:50:56 -0600

    ¿Cómo crear objetos con tipos específicos?

--- src/objetos/objects.ts
+++ src/objetos/objects.ts
@@ -1,19 +1,20 @@
 (()=>{
 
-    let flash = {
+    let flash: { name: string, age?: number, powers: string[] } = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Súper velocidad', 'Viajar en el tiempo']
     }
-
+ 
     flash = {
         name: 'Wally West',
         age: 16,
         powers: ['Lanzar rayos'],
-        getNombre(){
-            return this.nombre;
-        }
+        // getNombre(){
+        //     return this.name;
+        // }
     }
 
-
+    console.log(flash);
+    
 })();
