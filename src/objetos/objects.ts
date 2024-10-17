848833c8573ddc8f610c7ec437163888d67c5377
DiegoAaronMP <daaron.macias@gmail.com>
2024-10-17 16:43:57 -0600

    Objetos básicos

---
+++ src/objetos/objects.ts
@@ -0,0 +1,19 @@
+(()=>{
+
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Súper velocidad', 'Viajar en el tiempo']
+    }
+
+    flash = {
+        name: 'Wally West',
+        age: 16,
+        powers: ['Lanzar rayos'],
+        getNombre(){
+            return this.nombre;
+        }
+    }
+
+
+})();
