7dc40205a3c2875ad41f6f5251bce07a27f85ffd
DiegoAaronMP <daaron.macias@gmail.com>
2024-10-29 16:47:52 -0600

    Multiples tipos permitidos

---
+++ src/objetos/union-types.ts
@@ -0,0 +1,20 @@
+(()=>{
+    type Hero = {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName?: () => string;
+    }
+
+    let myCustomVariable: (string | number | Hero) = 'Diego';
+    console.log(typeof myCustomVariable);
+
+    myCustomVariable = 20;
+    console.log(typeof myCustomVariable);
+    
+    myCustomVariable = {
+        name: 'Bruce',
+        age: 43,
+        powers: ['Ninguno']
+    }
+})();
